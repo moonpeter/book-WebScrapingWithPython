3e34cd1a818f8e269d695ced3c5454f71d9f5a5c
moonpeter <moonpeter@kakao.com>
2020-07-14 13:56:24 +0900

    page 51 / ch02 끝

--- scrap.py
+++ scrap.py
@@ -1,9 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
-html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html, "html.parser")
+images = bsObj.findAll("img", {"src":re.compile("\.\.\/img\/gifts/img.*\.jpg")})
 
-nameList = bsObj.findAll("span", {"class":"green"})
-for name in nameList:
-    print(name.get_text())
+for image in images:
+    print(image["src"])
