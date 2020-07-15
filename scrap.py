d52bcb6bfa3f1dea1e0ec0ede20af3cf8c1868f2
moonpeter <moonpeter@kakao.com>
2020-07-15 13:29:14 +0900

    update

--- scrap.py
+++ scrap.py
@@ -1,10 +1,19 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import datetime
+import random
 import re
+random.seed(datetime.datetime.now())
 
-html = urlopen("http://www.pythonscraping.com/pages/page3.html")
-bsObj = BeautifulSoup(html, "html.parser")
-images = bsObj.findAll("img", {"src":re.compile("\.\.\/img\/gifts/img.*\.jpg")})
 
-for image in images:
-    print(image["src"])
+def getLinks(articleUrl):
+    html = urlopen("http://en.wikipedia.org"+articleUrl)
+    bsObj = BeautifulSoup(html, "html.parser")
+    return bsObj.find("div", {"id" : "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+
+
+links = getLinks("/wiki/Kevin_Bacon")
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+    print(newArticle)
+    links = getLinks(newArticle)
