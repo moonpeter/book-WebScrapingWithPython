aa26ceb3c03545a86e3787fb88ef5ebbd198fab5
kyoungho.moon <moonpeter@kakao.com>
2020-07-16 13:34:24 +0900

    http://orelly.com에서 시작해 외부 링크에서 외부 링크로 무작위로 이동합니다.

--- scrap.py
+++ scrap.py
@@ -1,19 +1,56 @@
 from urllib.request import urlopen
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import re
+import datetime
+import random
 
-page = set()
-def getLinks(pageUrl):
-    global pages
-    html = urlopen("http://en.wikipedia.org"+pageUrl)
+pages = set()
+random.seed(datetime.datetime.now())
+
+
+# 페이지에서 발견된 내부 링크를 모두 목록으로 만듭니다.
+def getInternalLinks(bsObj, includeUrl):
+    includeUrl = urlparse(includeUrl).scheme + "://" + urlparse(includeUrl).netloc
+    internalLinks = []
+    # /로 시작하는 링크를 모두 찾습니다.
+    for link in bsObj.findAll("a", href=re.compile("^(/|.*" + includeUrl + ")")):
+        if link.attrs['href'] is not None:
+            if (link.attrs['href'].startswith("/")):
+                internalLinks.append(includeUrl + link.attrs['href'])
+            else:
+                internalLinks.append(link.attrs['href'])
+    return internalLinks
+
+
+# 페이지에서 발견된 외부 링크를 모두 목록으로 만듭니다.
+def getExternalLinks(bsObj, excludeUrl):
+    externalLinks = []
+    # 현재 URL를 포함하지 않으면서 http나 www로 시작하는 링크를 모두 찾습니다.
+    for link in bsObj.findAll("a", href=re.compile("^(http|www)((?!"+excludeUrl+").)*$")):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in externalLinks:
+                externalLinks.append(link.attrs['href'])
+    return externalLinks
+
+
+def getRandomExternalLink(startingPage):
+    html = urlopen(startingPage)
     bsObj = BeautifulSoup(html, "html.parser")
-    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")):
-        if 'href' in Link.attrs:
-            if link.attrs['href'] not in pages:
-                # 새 페이지를 발견
-                newPage = link.attrs['href']
-                print(newPage)
-                pages.add(newPage)
-                getLinks(newPage)
+    externalLinks = getExternalLinks(bsObj, urlparse(startingPage).netloc)
+    if len(externalLinks) == 0:
+        domain = urlparse(startingPage).scheme+"://"+urlparse(startingPage).netloc
+        internalLinks = getInternalLinks(bsObj, domain)
+        return getRandomExternalLink(internalLinks[random.randint(0, len(internalLinks)-1)])
 
-getLinks("")
+    else:
+        return externalLinks[random.randint(0, len(externalLinks)-1)]
+
+
+def followExternalOnly(startingSite):
+    externalLink = getRandomExternalLink(startingSite)
+    print("Random external link is: " + externalLink)
+    followExternalOnly(externalLink)
+
+
+followExternalOnly("http://oreilly.com")
