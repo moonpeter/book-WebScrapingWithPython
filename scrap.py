7e81577a9734fda8a3b8b255235053c0aab72638
kyoungho.moon <moonpeter@kakao.com>
2020-07-17 12:59:59 +0900

    4.8 모든 것을 하나로 예제 : 위키백과 개정 내역 페이지를 크롤링하고 IP주소를 찾아내는 스크립트

--- scrap.py
+++ scrap.py
@@ -1,86 +1,40 @@
 from urllib.request import urlopen
-from urllib.parse import urlparse
 from bs4 import BeautifulSoup
-import re
 import datetime
 import random
+import re
 
-pages = set()
 random.seed(datetime.datetime.now())
 
-
-# 페이지에서 발견된 내부 링크를 모두 목록으로 만듭니다.
-def getInternalLinks(bsObj, includeUrl):
-    includeUrl = urlparse(includeUrl).scheme + "://" + urlparse(includeUrl).netloc
-    internalLinks = []
-    # /로 시작하는 링크를 모두 찾습니다.
-    for link in bsObj.findAll("a", href=re.compile("^(/|.*" + includeUrl + ")")):
-        if link.attrs['href'] is not None:
-            if (link.attrs['href'].startswith("/")):
-                internalLinks.append(includeUrl + link.attrs['href'])
-            else:
-                internalLinks.append(link.attrs['href'])
-    return internalLinks
-
-
-# 페이지에서 발견된 외부 링크를 모두 목록으로 만듭니다.
-def getExternalLinks(bsObj, excludeUrl):
-    externalLinks = []
-    # 현재 URL를 포함하지 않으면서 http나 www로 시작하는 링크를 모두 찾습니다.
-    for link in bsObj.findAll("a", href=re.compile("^(http|www)((?!" + excludeUrl + ").)*$")):
-        if link.attrs['href'] is not None:
-            if link.attrs['href'] not in externalLinks:
-                externalLinks.append(link.attrs['href'])
-    return externalLinks
-
-
-def getRandomExternalLink(startingPage):
-    html = urlopen(startingPage)
+def getLinks(articleUrl):
+    html = urlopen("http://en.wikipedia.org"+articleUrl)
     bsObj = BeautifulSoup(html, "html.parser")
-    externalLinks = getExternalLinks(bsObj, urlparse(startingPage).netloc)
-    if len(externalLinks) == 0:
-        domain = urlparse(startingPage).scheme + "://" + urlparse(startingPage).netloc
-        internalLinks = getInternalLinks(bsObj, domain)
-        return getRandomExternalLink(internalLinks[random.randint(0, len(internalLinks) - 1)])
+    return bsObj.find("div", {"id":"bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
 
-    else:
-        return externalLinks[random.randint(0, len(externalLinks) - 1)]
-
-
-def followExternalOnly(startingSite):
-    externalLink = getRandomExternalLink(startingSite)
-    print("Random external link is: " + externalLink)
-    followExternalOnly(externalLink)
-
-
-# 사이트에서 찾은 외부 URL을 모두 리스트로 수집
-allExtLinks = set()
-allIntLinks = set()
-
-
-def getAllExternalLinks(siteUrl):
-    html = urlopen(siteUrl)
+def getHistory(pageUrl):
+    # 개정 내역 페이지 URL은 다음과 같은 형식입니다.
+    # http://en.wikipedia.org/w/index.php?title=Title_in_URL&action=history
+    pageUrl = pageUrl.replace("/wiki/", "")
+    historyUrl = "http://en.wikipedia.org/w/index.php?title="
+    historyUrl += pageUrl + "&action=history"
+    print("history url is: "+historyUrl)
+    html = urlopen(historyUrl)
     bsObj = BeautifulSoup(html, "html.parser")
-    internalLinks = getInternalLinks(bsObj, splitAddress(domain)[0])
-    externalLinks = getExternalLinks(bsObj, splitAddress(domain)[0])
+    # 사용자명 대신 IP 주소가 담긴, 클래스가 mw-anonuserlink인 링크만 찾습니다.
+    ipAddresses = bsObj.findAll("a", {"class":"mw-anonuserlink"})
+    addressList = set()
+    for ipAddresses in ipAddresses:
+        addressList.add(ipAddresses.get_text())
+    return addressList
 
-    for link in externalLinks:
-        if link not in allExtLinks:
-            allExtLinks.add(link)
-            print(link)
-        for link in internalLinks:
-            if link == "/":
-                link = domain
-            elif link[0:2] == "//":
-                link = "http:" + link
-            elif link[0:1] == "/":
-                link = domain + link
+links = getLinks("/wiki/Python_(programming_language)")
 
-            if link not in allIntLinks:
-                print("About to get link: " + link)
-                allIntLinks.add(link)
-                getAllExternalLinks(link)
+while(len(links) > 0):
+    for link in links:
+        print("------------------")
+        historyIPs = getHistory(link.attrs["href"])
+        for historyIP in historyIPs:
+            print(historyIP)
 
-
-domain = "http://oreilly.com"
-getAllExternalLinks(domain)
+    newLink = links[random.randint(0, len(links)-1)].attrs["href"]
+    links = getLinks(newLink)
