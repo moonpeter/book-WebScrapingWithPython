c1901215b5a0a135a8207d3380592589ce26d73d
kyoungho.moon <moonpeter@kakao.com>
2020-07-22 13:54:01 +0900

    5.3.3 파이썬과의 통합

--- ch05-scrap.py
+++ ch05-scrap.py
@@ -1,8 +1,39 @@
-from urllib.request import urlretrieve
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
+import datetime
+import random
+import pymysql
 
-html = urlopen("http://www.pythonscraping.com")
-bsObj = BeautifulSoup(html, "html.parser")
-imageLocation = bsObj.find("a", {"id": "logo"}).find("img")["src"]
-urlretrieve(imageLocation, "logo.jpg")
+conn = pymysql.connect(host='localhost', user='root', password='', db='mysql', charset='utf8')
+
+cur = conn.cursor()
+cur.execute("USE scraping")
+
+random.seed(datetime.datetime.now())
+
+
+def store(title, content):
+    cur.execute(
+        "INSERT INTO pages (title, content) VALUES (\"%s\", \"%s\")", (title, content))
+    cur.connection.commit()
+
+
+def getLinks(articleUrl):
+    html = urlopen("http://en.wikipedia.org" + articleUrl)
+    bsObj = BeautifulSoup(html, "html.parser")
+    title = bsObj.find("h1").get_text()
+    content = bsObj.find("div", {"id": "mw-content-text"}).find("p").get_text()
+    store(title, content)
+    return bsObj.find("div", {"id": "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+
+
+links = getLinks("/wiki/Kevin_Bacon")
+try:
+    while len(links) > 0:
+        newArticle = links[random.randint(0, len(links) - 1)].attrs["href"]
+        print(newArticle)
+        links = getLinks(newArticle)
+finally:
+    cur.close()
+    conn.close()
--- twitter.py
+++
@@ -1,6 +0,0 @@
-from twitter import Twitter
-from twitter import *
-
-t = Twitter(auth=OAuth([Access Token], [Access Token secret], [Consumer Key], Consumer Secret))
-pythonTweets = t.search.tweets(q = "#python")
-print(pythonTweets)
