3c02d7e54a95d892c97e8694f94a266d76dc4d11
moonpeter <moonpeter@kakao.com>
2020-07-25 13:55:58 +0900

    5.4.2 뷰티풀숩에 데이터 넣기

--- ch_self/onemouth.py
+++ ch_self/onemouth.py
@@ -1,6 +1,9 @@
 import urllib.request
+import bs4
 
 url = "https://naver.com"
 html = urllib.request.urlopen(url)
 
-print(html.read())
+bs_obj = bs4.BeautifulSoup(html, "html.parser")
+
+print(bs_obj)
