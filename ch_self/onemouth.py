388e2fe09aaaec2cd896838b23a25d042c85d86e
kyoungho.moon <moonpeter@kakao.com>
2020-07-27 13:19:55 +0900

    네이버 웹툰(윈드브페이커) 제목/링크/이미지 뽑기 --- up 표시때문에 이미지 순서는 맞지 않음

--- ch_self/onemouth.py
+++ ch_self/onemouth.py
@@ -1,16 +1,25 @@
-import urllib.request
-
+import requests
 import bs4
 
-url = "https://news.naver.com/"
-html = urllib.request.urlopen(url)
+url = "https://comic.naver.com/webtoon/list.nhn?titleId=602910&weekday=mon"
 
-bs_obj = bs4.BeautifulSoup(html, "html.parser")
-div = bs_obj.find("div", {"class": "main_component droppable", "id": "section_politics"})
-strong = div.findAll("strong")
+results = requests.get(url=url)
+bs_obj = bs4.BeautifulSoup(results.content, "html.parser")
 
-for s_tag in strong:
-    print(s_tag.text)
-    print(s_tag.text)
-    print(s_tag.text)
-    print(s_tag.text)
+tables = bs_obj.findAll("td", {"class": "title"})
+hrefs = [td.find("a")["href"] for td in tables]
+
+titles = [td.find("a") for td in tables]
+
+viewList = bs_obj.find('table', {'class': "viewList"})
+img_in_viewList = viewList.find_all('img')
+imgs = []
+for j in range(len(img_in_viewList)):
+    imgs.append(((img_in_viewList)[j])['src'])
+
+for i in range(len(hrefs)):
+    print('---------------------------------------------------')
+    print('title : ' + titles[i].text)
+    print('links : ' + 'https://comic.naver.com' + hrefs[i])
+    print('image : ' + imgs[i])
+    i += 1
