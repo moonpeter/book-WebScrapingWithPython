0620059839c5fb99533150d76296dce98fa04c7b
kyoungho.moon <moonpeter@kakao.com>
2020-07-27 13:29:27 +0900

    네이버 웹툰(윈드브페이커) 제목/링크/이미지 뽑기 --- up 표시때문에 이미지 순서는 맞지 않음 + 2페이지로 이동하여 크롤링

--- ch_self/onemouth.py
+++ ch_self/onemouth.py
@@ -1,25 +1,29 @@
 import requests
 import bs4
 
-url = "https://comic.naver.com/webtoon/list.nhn?titleId=602910&weekday=mon"
 
-results = requests.get(url=url)
-bs_obj = bs4.BeautifulSoup(results.content, "html.parser")
+def webtoon(url):
+    results = requests.get(url=url)
+    bs_obj = bs4.BeautifulSoup(results.content, "html.parser")
 
-tables = bs_obj.findAll("td", {"class": "title"})
-hrefs = [td.find("a")["href"] for td in tables]
+    tables = bs_obj.findAll("td", {"class": "title"})
+    hrefs = [td.find("a")["href"] for td in tables]
 
-titles = [td.find("a") for td in tables]
+    titles = [td.find("a") for td in tables]
 
-viewList = bs_obj.find('table', {'class': "viewList"})
-img_in_viewList = viewList.find_all('img')
-imgs = []
-for j in range(len(img_in_viewList)):
-    imgs.append(((img_in_viewList)[j])['src'])
+    view_list = bs_obj.find('table', {'class': "viewList"})
+    img_in_view_list = view_list.find_all('img')
+    imgs = []
+    for j in range(len(img_in_view_list)):
+        imgs.append(((img_in_view_list)[j])['src'])
 
-for i in range(len(hrefs)):
-    print('---------------------------------------------------')
-    print('title : ' + titles[i].text)
-    print('links : ' + 'https://comic.naver.com' + hrefs[i])
-    print('image : ' + imgs[i])
-    i += 1
+    for i in range(len(hrefs)):
+        print('---------------------------------------------------')
+        print('title : ' + titles[i].text)
+        print('links : ' + 'https://comic.naver.com' + hrefs[i])
+        print('image : ' + imgs[i])
+        i += 1
+
+
+webtoon('https://comic.naver.com/webtoon/list.nhn?titleId=602910&weekday=mon')
+webtoon('https://comic.naver.com/webtoon/list.nhn?titleId=602910&weekday=mon&page=2')
